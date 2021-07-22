9fffe95c8dd0be912e29131bc75c44662aa0aabf
JM941935 <78488466+JM941935@users.noreply.github.com>
2021-07-22 14:38:03 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -5,31 +5,29 @@ import geni.rspec.igext as IG
 pc = portal.Context()
 request = pc.makeRequestRSpec()
 
-tourDescription = \
-
-"""
-This profile provides a two-node set to study SSO. One node will be the LDAP server, and the other node is a client who is to be 
-authenticated using LDAP. 
-"""
-
+tourDescription = "2-node SSO setup (LDAP server & client)."
 tour = IG.Tour()
-tour.Description(IG.Tour.TEXT,tourDescription)
+tour.Description(IG.Tour.TEXT, tourDescription)
 request.addTour(tour)
-prefixForIP = "192.168.1."
 link = request.LAN("lan")
 
-for i in range(2):
-	if i == 0:
-		node = request.XenVM("ldapserver")    
-	else:
-		node = request.XenVM("ldapclient")
+# 
+node1 = request.XenVM("ldapserver")    
+node1.routable_control_ip = "true"  
+node1.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD"
+iface1 = node1.addInterface("if0")
+iface1.component_id = "eth1"
+iface1.addAddress(pg.IPv4Address("192.168.1.1", "255.255.255.0"))
+link.addInterface(iface1)
 
-	node.routable_control_ip = "true"  
-	node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD"
-	iface = node.addInterface("if" + str(i))
-	iface.component_id = "eth1"
-	iface.addAddress(pg.IPv4Address(prefixForIP + str(i + 1), "255.255.255.0"))
-	link.addInterface(iface)
+# 
+node2 = request.XenVM("ldapclient")
+node2.routable_control_ip = "true"  
+node2.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD"
+iface2 = node2.addInterface("if1")
+iface2.component_id = "eth1"
+iface2.addAddress(pg.IPv4Address("192.168.1.2", "255.255.255.0"))
+link.addInterface(iface2)
 
 # Print the RSpec to the enclosing page.
 pc.printRequestRSpec(request)
