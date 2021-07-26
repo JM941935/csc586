e2502d884e56be2789bfdca7e72c0232a0b01fc8
JM941935 <78488466+JM941935@users.noreply.github.com>
2021-07-26 14:55:29 -0400

    Add files via upload

---
+++ nfsServerSetup.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# 
+echo 'installing software...'
+sudo apt-get remove -y --purge man-db
+sudo apt-get update
+sudo apt install -y nfs-kernel-server
+
+# 
+echo 'making share directory...'
+mkdir '/var/webserver_monitor'
+chown -R nobody:nogroup '/var/webserver_monitor'
+chmod 777 '/var/webserver_monitor'
+
+# 
+echo 'editing nfs config @ /etc/exports...'
+echo '/var/webserver_monitor 192.168.1.1(rw,sync,no_root_squash,no_subtree_check)' >> '/etc/exports'
+
+# 
+echo 'configuring firewall...'
+ufw enable
+ufw allow from 192.168.1.1 to any port nfs
+
+# 
+echo 'restarting nfs-kernel-server...'
+exportfs -a
+systemctl restart nfs-kernel-server && sleep 1
