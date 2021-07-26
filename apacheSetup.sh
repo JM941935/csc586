53bc988830dfbbae72d4da7e5bb0842b022b3e7e
JM941935 <78488466+JM941935@users.noreply.github.com>
2021-07-26 14:40:17 -0400

    Update apacheSetup.sh

--- apacheSetup.sh
+++ apacheSetup.sh
@@ -3,20 +3,9 @@
 # remove man-db because its slow
 echo 'removing man-db...'
 apt-get remove -y --purge man-db
-apt-get install debconf-utils
-
-# add selections to debconf database
-echo 'adding selections to debconf database...'
-bash -c 'echo "mysql-community-server mysql-community-server/re-root-pass password 123" | debconf-set-selections'
-bash -c 'echo "mysql-community-server mysql-community-server/remove-data-dir boolean false" | debconf-set-selections'
-bash -c 'echo "mysql-community-server mysql-community-server/root-pass password 123" | debconf-set-selections'
-bash -c 'echo "phpmyadmin phpmyadmin/app-password-confirm password 123" | debconf-set-selections'
-bash -c 'echo "phpmyadmin phpmyadmin/dbconfig-install boolean true" | debconf-set-selections'
-bash -c 'echo "phpmyadmin phpmyadmin/mysql/admin-pass password 123" | debconf-set-selections'
-bash -c 'echo "phpmyadmin phpmyadmin/mysql/app-pass password 123" | debconf-set-selections'
-bash -c 'echo "phpmyadmin phpmyadmin/password-confirm password 123" | debconf-set-selections'
-bash -c 'echo "phpmyadmin phpmyadmin/reconfigure-webserver select apache2" | debconf-set-selections'
-bash -c 'echo "phpmyadmin phpmyadmin/setup-password password 123" | debconf-set-selections'
 
 # 
-wget https://raw.githubusercontent.com/JM941935/csc586/Assignment-2/lamp.sh -O /users/JM941935/lamp.sh
+echo 'installing packages...'
+apt-get update
+apt-get install -y apache2 php libapache2-mod-php php-cli php-mysql php-cgi php-curl php-json php-apcu php-gd php-xml php-mbstring php-gettext
+systemctl start apache2 && sleep 1
