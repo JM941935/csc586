99c0d7ca10979e65ca907d82864e2ee8d4b07ef4
JM941935 <78488466+JM941935@users.noreply.github.com>
2021-07-26 15:21:07 -0400

    Update apacheSetup.sh

--- apacheSetup.sh
+++ apacheSetup.sh
@@ -7,5 +7,13 @@ apt-get remove -y --purge man-db
 # 
 echo 'installing packages...'
 apt-get update
-apt-get install -y apache2 php libapache2-mod-php php-cli php-mysql php-cgi php-curl php-json php-apcu php-gd php-xml php-mbstring php-gettext
+apt-get install -y apache2 php libapache2-mod-php php-cli php-mysql php-cgi php-curl php-json php-apcu php-gd php-xml php-mbstring php-gettext nfs-common
 systemctl start apache2 && sleep 1
+
+# 
+echo 'creating /var/webserver_monitor...'
+mkdir '/var/webserver_monitor'
+
+#
+echo 'mounting /var/webserver_monitor...'
+mount 192.168.1.2:/var/webserver_monitor /var/webserver_monitor
