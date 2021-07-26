2291f01fe774bdf83a756feaee99f7a64ca6c05d
JM941935 <78488466+JM941935@users.noreply.github.com>
2021-07-26 14:56:16 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -28,7 +28,7 @@ link.addInterface(iface0)
 node1 = request.XenVM("observer")
 node1.routable_control_ip = "false"
 node1.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD"
-node0.addService(pg.Execute(shell="sh", command="bash /local/repository/nfsserver.sh"))
+node0.addService(pg.Execute(shell="sh", command="bash /local/repository/nfsServerSetup.sh"))
 iface1 = node1.addInterface("if1")
 iface1.component_id = "eth1"
 iface1.addAddress(pg.IPv4Address("192.168.1.2", "255.255.255.0"))
