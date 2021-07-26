eb4db2387452abdcc83c6ab66af6088f80bdd4fd
JM941935 <78488466+JM941935@users.noreply.github.com>
2021-07-26 15:24:34 -0400

    Add files via upload

---
+++ monitor.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+# On observer, create a new script called monitor.sh to monitor /var/webserver_monitor/unauthorized.log.
+# If there are new entries, an email should be sent to the admin with the content of these new entries.
+# Otherwise, the email simply says "No unauthorized access."
+# Create a cron job that runs monitor.sh every hour.
+
+# install mailutils if its not already installed
+dpkg -s "mailutils" &> /dev/null
+if [ ! $? -eq 0 ]; then
+    apt-get install mailutils
+fi
+
+# if email.txt exists, get the date from the last line and create a backup
+# else set the date to 0
+if [ -f "./email.txt" ]; then
+
+    # get date of the last record sent to the admin
+    LASTLINE=$(tail -1 "./email.txt")
+    EDATE=$(echo "$LASTLINE" | sed -r 's/^.*([0-9]{14})$/\1/')
+
+    # create a temporary backup
+    mv "./email.txt" "./email_bak.txt"
+else
+
+    # will match every line in unauthorized.log
+    EDATE=0
+fi
+
+# echo each line with a greater date into a new email.txt file
+cat "/var/webserver_monitor/unauthorized.log" |  while read LINE; do
+
+    # get the date string from the end of the line
+    LDATE=$(echo "$LINE" | sed -r 's/^.*([0-9]{14})$/\1/')
+
+    # if the line date > last record sent, it is new, echo into email.txt
+    if (( "$LDATE" > "$EDATE" )); then
+        echo "$LINE" >> "./email.txt" 
+    fi
+done
+
+# if email.txt exists, send its contents to the admin
+# else send an email with no body
+TO="DOITLIVE.localdomain"
+if [ -f "./email.txt" ]; then
+
+    # email the contents of email.txt to the admin
+    SUBJECT="Unauthorized access reported"
+    mail -s "$SUBJECT" "$TO" < $MESSAGE
+
+    # remove the backup
+    rm "./email_bak.txt"
+else
+    # email admin with just a subject line
+    SUBJECT="No unauthorized access"
+    mail -s "$SUBJECT" "$TO" < ""
+
+    # rename the backup and leave it
+    mv "./email_bak.txt" "./email.txt"
+fi
+
+# add cron job to run this script every hour
+(crontab -l | grep -v -F "/monitor.sh";echo "0 * * * * /usr/local/sbin/monitor.sh") | crontab -
---
+++ scan.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# On webserver, Create a shell script called scan.sh that will scan the webserver's auth.log file for unauthorized/failed SSH access. 
+# Based on the IP information, scan.sh should also identifies the country of origin of this IP address automatically. 
+# The resulting information should be appended to a file called unauthorized.log inside 
+# the NFS-mounted /var/webserver_log with the following format: IP_ADDRESS COUNTRY DATE.
+# Create a cron job that will execute this script every 5 minutes.
+
+# install geoip if its not already installed
+dpkg -s "geoip-bin" &> /dev/null
+if [ ! $? -eq 0 ]; then
+    apt-get install geoip-bin
+fi
+
+# if webserver_log doesnt exist, make it
+if [ ! -d "/var/webserver_log" ]; then
+    mkdir "/var/webserver_log"
+fi
+
+# declare in and out file paths
+INLOG="/var/log/auth.log"
+OUTLOG="/var/webserver_log/unauthorized.log"
+
+# extract ip, country, and date from each matching line
+cat "$INLOG" | grep "Invalid user" | while read LINE; do
+
+    # extract IP address
+    IP=$(echo "$LINE" | sed -r 's/^.*([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}).*$/\1/')
+
+    # get country with geoiplookup
+    COUNTRY=$(geoiplookup $IP)
+
+    # extract and convert date to format YyyyMmDdHhMmSs (24 hour clock)
+    DATESTRING=$(echo "$LINE" | sed -r 's/^(.*)localhost.*/\1/')
+    DATE=$(date -d "$DATESTRING" +'%Y%m%d%H%M%S')
+
+    # echo result into unauthorized.log
+    echo "$IP ${COUNTRY:23} $DATE" >> "$OUTLOG"
+done
+
+# add cron job to run this script every 5 minutes
+(crontab -l | grep -v -F "/scan.sh";echo "*/5 * * * * /usr/local/sbin/scan.sh") | crontab -
