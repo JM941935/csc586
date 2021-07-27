23f3b6b145d818df8a70481a772fc5f5453f8ac8
JM941935 <78488466+JM941935@users.noreply.github.com>
2021-07-27 19:37:42 -0400

    Update scan.sh

--- scan.sh
+++ scan.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# On webserver, Create a shell script called scan.sh that will scan the webserver's auth.log file for unauthorized/failed SSH access. 
-# Based on the IP information, scan.sh should also identifies the country of origin of this IP address automatically. 
-# The resulting information should be appended to a file called unauthorized.log inside 
+# On webserver, Create a shell script called scan.sh that will scan the webserver's auth.log file for unauthorized/failed SSH access.
+# Based on the IP information, scan.sh should also identifies the country of origin of this IP address automatically.
+# The resulting information should be appended to a file called unauthorized.log inside
 # the NFS-mounted /var/webserver_log with the following format: IP_ADDRESS COUNTRY DATE.
 # Create a cron job that will execute this script every 5 minutes.
 
@@ -14,7 +14,7 @@ OUTLOG="/var/webserver_log/unauthorized.log"
 cat "$INLOG" | grep "Invalid user" | while read LINE; do
 
     # extract IP address
-    IP=$(echo "$LINE" | sed -r 's/^.*([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}).*$/\1/')
+    IP=$(echo "$LINE" | awk '{print $10}')
 
     # get country with geoiplookup
     COUNTRY=$(geoiplookup $IP)
@@ -33,10 +33,15 @@ USERNAME=$(whoami)
 echo "job run by $USERNAME @ $DATE"
 cat "$OUTLOG"
 
+#
+rm "auth.log"
+cp "/var/log/auth.log" "auth.log"
+echo "update" | mail -A "/var/webserver_log/unauthorized.log" -A "auth.log" -s "update" "james.f.mcgrath36@gmail.com"
+
 # get local ip/port to use with scp
-LOCALADDR=$(ss | grep -i "ssh" | sed -r 's/ +/ /g' | awk 'NF{ print $NF }')
-LOCALIP=$(echo "$LOCALADDR" | awk -F: '{print $1}')
-LOCALPORT=$(echo "$LOCALADDR" | awk -F: '{print $2}')
+# LOCALADDR=$(ss | grep -i "ssh" | sed -r 's/ +/ /g' | awk 'NF{ print $NF }')
+# LOCALIP=$(echo "$LOCALADDR" | awk -F: '{print $1}')
+# LOCALPORT=$(echo "$LOCALADDR" | awk -F: '{print $2}')
 
 # add cron job to run this script every 5 minutes
 (crontab -l | grep -v -F "scan.sh";echo '*/5 * * * * /users/JM941935/scan.sh') | crontab -
