fea78a7942469536dc255afc64adaf8bcf26dbae
JM941935 <78488466+JM941935@users.noreply.github.com>
2021-07-30 16:39:19 -0400

    Update webserverSetup.sh

--- webserverSetup.sh
+++ webserverSetup.sh
@@ -1,22 +1,8 @@
 #!/bin/bash
 
-# owner(u)group(g)all(a)
-# 0	---
-# 1	--x
-# 2	-w-
-# 3	-wx
-# 4	r--
-# 5	r-x
-# 6	rw-
-# 7	rwx
-
-# 
-wget https://raw.githubusercontent.com/JM941935/csc586/Assignment-2/scan.sh -O /users/JM941935/scan.sh
-chmod 777 /users/JM941935/scan.sh
-
 # remove man-db because its slow
 echo 'removing man-db'
-apt-get remove -y --purge man-db
+apt-get remove -y --purge man-db && apt-get install man-db
 
 # 
 echo 'installing packages'
@@ -25,15 +11,13 @@ apt-get install -y apache2 php libapache2-mod-php php-cli php-mysql php-cgi php-
 systemctl start apache2 && sleep 1
 
 # 
-chmod 644 '/var/log' && echo 'changed permissions on /var/log to 644'
-(bash -c 'touch /var/log/auth.log' && sleep 1) && echo 'created /var/log/auth.log'
-if [[ -f '/var/log/auth.log' ]]; then (chmod 644 '/var/log/auth.log' && echo 'changed permissions on /var/log/auth.log to 644'); fi
+if [[ ! -f '/var/log/auth.log' ]]; then (bash -c 'touch /var/log/auth.log' && sleep 1) && echo 'created /var/log/auth.log'; fi
 
 # set loglevel to verbose
-echo 'setting LogLevel to VERBOSE'
-sed -i -r 's/^#(SyslogFacility AUTH)/\1/g' '/etc/ssh/sshd_config' && echo 'SyslogFacility set to AUTH'
-sed -i -r 's/^#(LogLevel) INFO/\1 VERBOSE/g' '/etc/ssh/sshd_config' && echo 'LogLevel set to VERBOSE'
-systemctl restart sshd && sleep 1
+# echo 'setting LogLevel to VERBOSE'
+# sed -i -r 's/^#(SyslogFacility AUTH)/\1/g' '/etc/ssh/sshd_config' && echo 'SyslogFacility set to AUTH'
+# sed -i -r 's/^#(LogLevel) INFO/\1 VERBOSE/g' '/etc/ssh/sshd_config' && echo 'LogLevel set to VERBOSE'
+# systemctl restart sshd && sleep 1
 
 # 
 echo 'creating /var/webserver_log'
