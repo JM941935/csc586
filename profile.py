21d92f176301cb6b0f7bcb1f37270668052ff613
JM941935 <78488466+JM941935@users.noreply.github.com>
2021-07-30 14:04:54 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -17,7 +17,6 @@ link = request.LAN("lan")
 node0 = request.XenVM("webserver")
 node0.routable_control_ip = "true"
 node0.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD"
-node0.addService(pg.Execute(shell="sh", command="bash /local/repository/webserverSetup.sh"))
 iface0 = node0.addInterface("if0")
 iface0.component_id = "eth1"
 iface0.addAddress(pg.IPv4Address("192.168.1.1", "255.255.255.0"))
@@ -28,7 +27,6 @@ link.addInterface(iface0)
 node1 = request.XenVM("observer")
 node1.routable_control_ip = "false"
 node1.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD"
-node0.addService(pg.Execute(shell="sh", command="bash /local/repository/observerSetup.sh"))
 iface1 = node1.addInterface("if1")
 iface1.component_id = "eth1"
 iface1.addAddress(pg.IPv4Address("192.168.1.2", "255.255.255.0"))
