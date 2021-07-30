adaf66102a9e2844861d8b83fd2f2a10bf6a629d
JM941935 <78488466+JM941935@users.noreply.github.com>
2021-07-30 14:04:07 -0400

    Update scan.sh

--- scan.sh
+++ scan.sh
@@ -9,34 +9,37 @@
 # declare in and out file paths, variables
 INLOG="/var/log/auth.log"
 OUTLOG="/var/webserver_log/unauthorized.log"
-OLDLOGSIZE=$(wc -l "$OUTLOG" | awk '{print $1}')
 LASTLOGGED=$(tail -1 "$OUTLOG" | awk '{print $3}')
 if [ -z "$LASTLOGGED" ]; then LASTLOGGED=0; fi
-TOTALSSHATTEMPTS=$(cat "$INLOG" | grep -c "Invalid user")
-EXECUTOR=$(whoami)
-EXECUTEDDATE=$(date +'%Y%m%d%H%M%S')
 
 # extract ip, country, and date from each matching line
-cat "$INLOG" | grep -E 'Invalid user)|(Disconnected from authenticating user root)' | while read LINE; do
+# 
+# the string "Invalid user" is sufficuent to account for every ssh login attempt, except when the attacker
+# tries to login with the username root. I included the string "Disconnected from authenticating user root"
+# to account for this. So, between these two strings, every individual attempt can be extracted from auth.log
+# without changing the loglevel from INFO to VERBOSE.
+cat "$INLOG" | grep -E '(Invalid user)|(Disconnected from authenticating user root)' | while read LINE; do
+
+    # get date
     DATESTRING=$(echo "$LINE" | awk '{printf "%s %s %s", $1, $2, $3}')
     DATE=$(date -d "$DATESTRING" +'%Y%m%d%H%M%S') # format YyyyMmDdHhMmSs (24 hour clock)
 
-    # if date > lastlogged, print to log
+    # if date > last entry in log, print
     if (( "$DATE" > "$LASTLOGGED" )); then
+
+        # get IP
         IP=$(echo "$LINE" | grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}')
+
+        # get country
         COUNTRY=$(geoiplookup $IP | cut -c24-25)
 
         # if country == IP, IP was not found in db
         if [[ "$COUNTRY" == "IP" ]]; then COUNTRY='??'; fi
+
+        # print
         echo "$IP $COUNTRY $DATE" >> "$OUTLOG"
     fi
 done
-
-# print job info to log
-NEWLOGSIZE=$(wc -l "$OUTLOG" | awk '{print $1}')
-NEWSSHATTEMPTS=$(($NEWLOGSIZE-$OLDLOGSIZE))
-echo "$EXECUTOR $EXECUTEDDATE $TOTALSSHATTEMPTS $NEWLOGSIZE $OLDLOGSIZE $NEWSSHATTEMPTS" >> scan.log
-cat "scan.log"
 
 # add cron job to run this script every 5 minutes
 (crontab -l | grep -v -F "scan.sh"; echo '*/5 * * * * /users/JM941935/scan.sh') | crontab -
